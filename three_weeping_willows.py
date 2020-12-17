224c35085b9327342f2eb2e8a8392f0b770102aa
Christopher Merrill <Christopher.Merrill@amobee.com>
2020-12-17 21:47:33 +0000

    Migrated to Python3 & Added MatPlotLib

--- three_weeping_willows.py
+++ three_weeping_willows.py
@@ -1,3 +1,8 @@
+# library
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def initialize():
 	global x, result
 	x = 0
@@ -12,8 +17,28 @@ def update():
 	x = (x + (4464*3)) * 0.9
 
 initialize()
-for t in xrange(30):
+for t in range(30):
 	update()
 	observe()
 
-print result
+print(result)
+
+
+# create data
+x=range(31)
+ 
+# stem function: first way
+plt.stem(x, result)
+plt.ylim(0, 120000)
+plt.show()
+ 
+# stem function: If no X provided, a sequence of numbers is created by python:
+plt.stem(values)
+#plt.show()
+ 
+# stem function: second way
+(markerline, stemlines, baseline) = plt.stem(x, result)
+plt.setp(baseline, visible=False)
+#plt.show()
+
+
